4010922717f74e5541922cfb0fe16c63f377c06d
hobbyscripterII <wruomma2@naver.com>
2024-01-11 14:43:01 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/baby/babycareproductsshop/BabyCareProductsShopApplication.java
@@ -0,0 +1,13 @@
+package com.baby.babycareproductsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyCareProductsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyCareProductsShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baby/babycareproductsshop/BabyCareProductsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baby.babycareproductsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyCareProductsShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
